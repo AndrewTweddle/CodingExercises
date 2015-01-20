b5b2daf2cad5138764fd5a35f440be7b667b9dcb
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-01-20 21:29:41 +0200

    Exercise 1.1 solution
    Solution to exercise 1.1 of CTCI

---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter1/Exercise1/WhenCheckingAStringForDuplicateLetters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AndrewTweddle.CodingExercises.CTCI.Chapter1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Tests.Chapter1.Exercise1
+{
+    [TestClass]
+    public class WhenCheckingAStringForDuplicateLetters
+    {
+        [TestMethod]
+        public void ThenAnEmptyStringHasNoDuplicates()
+        {
+            Assert.IsFalse(Exercise1_1.HasDuplicateLetters(String.Empty));
+        }
+
+        [TestMethod]
+        public void ThenASingleCharStringHasNoDuplicates()
+        {
+            Assert.IsFalse(Exercise1_1.HasDuplicateLetters("a"));
+        }
+
+        [TestMethod]
+        public void ThenAStringWithDifferentCapitalisationsOfTheSameLetterHasNoDuplicates()
+        {
+            Assert.IsFalse(Exercise1_1.HasDuplicateLetters("Aa"));
+        }
+
+        [TestMethod]
+        public void ThenAStringWithTheSameLetterAndCapitalisationHasDuplicates()
+        {
+            Assert.IsTrue(Exercise1_1.HasDuplicateLetters("aba"));
+             // Put the letters at the edge of the string to test boundary behaviour
+        }
+
+        [TestMethod]
+        public void ThenAStringWithAllDifferentLettersHasNoDuplicates()
+        {
+            Assert.IsFalse(Exercise1_1.HasDuplicateLetters("abcde"));
+        }
+    }
+}
---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI/Chapter1/Exercise1_1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Chapter1
+{
+    public static class Exercise1_1
+    {
+        /// <summary>
+        /// This method checks for duplicates letters 
+        /// by using a HashSet to track letters previously found
+        /// </summary>
+        /// <param name="letters">The string of letters to check for duplicate letters</param>
+        /// <returns>A boolean value indicating whether any duplicate letters were found</returns>
+        public static bool HasDuplicateLetters(string letters)
+        {
+            HashSet<char> prevChars = new HashSet<char>();
+            foreach (char ch in letters)
+            {
+                if (prevChars.Contains(ch)) return true;
+                prevChars.Add(ch);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This method does not use any intermediate storage 
+        /// to determine whether the string has duplicate letters.
+        /// </summary>
+        /// <param name="letters">The string of letters to check for duplicate letters</param>
+        /// <returns>A boolean value indicating whether any duplicate letters were found</returns>
+        public static bool HasDuplicateLetters2(string letters)
+        {
+            int length = letters.Length;
+            for (int i = 0; i < length - 1; i++)
+                for (int j = i + 1; j < length; j++)
+                    if (letters[j] == letters[i])
+                        return true;
+            return false;
+        }
+    }
+}
