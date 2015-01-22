2b9faceee61cddaaab4e2be6e2700d87bd995132
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-01-22 21:40:27 +0200

    CTCI Ex 2.1: remove dup letters from linked list

---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter2/Exercise1/WhenRemovingDuplicateLetters.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Tests.Chapter2.Exercise1
+{
+    [TestClass]
+    public class WhenRemovingDuplicateLetters
+    {
+        private string ConvertToString(LinkedList<char> letters)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char letter in letters)
+            {
+                sb.Append(letter);
+            }
+            return sb.ToString();
+        }
+
+        private LinkedList<char> ConvertToLinkedList(string s)
+        {
+            LinkedList<char> linkedList = new LinkedList<char>();
+            foreach (char ch in s)
+            {
+                linkedList.AddLast(ch);
+            }
+            return linkedList;
+        }
+
+        private void CheckDeduplication(string input, string expectedOutput)
+        {
+            // Arrange:
+            LinkedList<char> linkedList = ConvertToLinkedList(input);
+
+            // Act:
+            DuplicateLetterRemover.RemoveDuplicateLetters(linkedList);
+
+            // Assert:
+            Assert.AreEqual(ConvertToString(linkedList), expectedOutput);
+        }
+
+        [TestMethod]
+        public void ThenAnEmptyListIsUnchanged()
+        {
+            LinkedList<char> linkedList = new LinkedList<char>();
+            DuplicateLetterRemover.RemoveDuplicateLetters(linkedList);
+            Assert.AreEqual(ConvertToString(linkedList), String.Empty);
+        }
+
+        [TestMethod]
+        public void ThenTheSameLetterWithDifferentCapitalisationIsNotRemoved()
+        {
+            CheckDeduplication("aA", "aA");
+        }
+
+        [TestMethod]
+        public void ThenTheSameLetterTwiceIsDeDuplicated()
+        {
+            CheckDeduplication("aa", "a");
+        }
+
+        [TestMethod]
+        public void ThenTheSameLetterInDifferentPartsOfLinkedListIsDeDuplicated()
+        {
+            CheckDeduplication("abaca", "abc");
+        }
+
+        [TestMethod]
+        public void ThenMultipleSetsOfDuplicateLettersAreDeDuplicated()
+        {
+            CheckDeduplication("abccbba", "abc");
+        }
+
+        [TestMethod]
+        public void ThenNoDeduplicationOccursIfAllLettersAreDifferent()
+        {
+            CheckDeduplication("abcde", "abcde");
+        }
+    }
+}
---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI/Chapter2/Exercise1/DuplicateLetterRemover.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise1
+{
+    public static class DuplicateLetterRemover
+    {
+        public static void RemoveDuplicateLetters(LinkedList<char> letters)
+        {
+            ISet<char> charsFound = new HashSet<char>();
+            LinkedListNode<char> currNode = letters.First;
+            while (currNode != null)
+            {
+                LinkedListNode<char> nextNode = currNode.Next;
+                char currChar = currNode.Value;
+                if (charsFound.Contains(currChar))
+                {
+                    letters.Remove(currNode);
+                }
+                else
+                {
+                    charsFound.Add(currChar);
+                }
+                currNode = nextNode;
+            }
+        }
+    }
+}
