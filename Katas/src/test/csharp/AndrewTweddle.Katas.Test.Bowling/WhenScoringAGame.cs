419dc2469f5ac88f06b709960faa7cb56456a77e
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-23 13:42:15 -0400

    Kata: Bowling: Test strikes and spares

--- Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringAGame.cs
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringAGame.cs
@@ -15,5 +15,25 @@ namespace AndrewTweddle.Katas.Test.Bowling
             int actualScore = BowlingScorer.Calculate(symbols);
             Assert.AreEqual(expectedScore, actualScore);
         }
+
+        [TestMethod]
+        public void ThenARepeatingStrikeFollowedByTwoZeroesScores50()
+        {
+            string symbols = new string('~', 5).Replace("~", "X--");
+            // i.e. A strike in one frame, and two zeroes in the next, repeated
+            int expectedScore = 50;
+            int actualScore = BowlingScorer.Calculate(symbols);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void ThenKnockingDown5PinsOnEveryThrowScores150()
+        {
+            string symbols = new string('~', 10).Replace("~", "5/") + "5";
+            // i.e. Two 5's in all frames, except three 5's in the tenth.
+            int expectedScore = 15*10;
+            int actualScore = BowlingScorer.Calculate(symbols);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
     }
 }
