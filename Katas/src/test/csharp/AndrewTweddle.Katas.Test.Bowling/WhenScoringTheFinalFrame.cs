3e853364b8607b234a62ededa0fbb7675a025cf0
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-22 22:26:52 -0400

    Bowling: Unit tests for tenth frame complications
    I had hoped that I wouldn't need to calculate frames.
    
    But I realised that wasn't possible, as an ending pattern like "X??"
    where the ?'s are not '/' or 'X', could be for the ninth frame or the
    ninth and tenth frame.
    
    Add unit tests to cover these cases.

--- Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringTheFinalFrame.cs
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringTheFinalFrame.cs
@@ -7,6 +7,8 @@ namespace AndrewTweddle.Katas.Test.Bowling
     [TestClass]
     public class WhenScoringTheFinalFrame
     {
+        #region Check that bonus throws in final frame are only counted once
+
         [TestMethod]
         public void ThenExtraThrowsDontScoreForBeingAStrike()
         {
@@ -15,5 +17,56 @@ namespace AndrewTweddle.Katas.Test.Bowling
             int actualScore = BowlingScorer.Calculate(throwChars);
             Assert.AreEqual(expectedScore, actualScore);
         }
+
+        [TestMethod]
+        public void ThenExtraThrowsDontScoreForBeingASpare()
+        {
+            String throwChars = new string('-', 18) + "x3/";
+            int expectedScore = 20;  // Zeroes for 9 frames, final spare has no subsequent throw to increase
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        #endregion
+
+        #region Count throws correctly based on whether a strike/spare in ninth
+
+        [TestMethod]
+        public void ThenTheTenthFrameThrowsAreAddedTwiceIfThereWasAStrikeOnTheNinthFrame()
+        {
+            String throwChars = new string('-', 16) + "x33";
+            int expectedScore = 22;  // Ninth + tenth: (10 + 3 + 3) + (3 + 3)
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void ThenTheTenthFrameThrowsAreAddedOnceIfThereWasNotAStrikeOnTheNinthFrame()
+        {
+            String throwChars = new string('-', 18) + "x33";
+            int expectedScore = 16;  // Tenth: (10 + 3 + 3)
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void ThenTheFirstThrowOfTheTenthFrameIsAddedTwiceIfThereWasASpareOnTheNinthFrame()
+        {
+            String throwChars = new string('-', 14) + "4/33";
+            int expectedScore = 19;  // Ninth + tenth: (4 + 6 + 3) + (3 + 3)
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        [TestMethod]
+        public void ThenTheFirstThrowOfTheTenthFrameIsAddedOnceIfThereWasNotASpareOnTheNinthFrame()
+        {
+            String throwChars = new string('-', 14) + "4533";
+            int expectedScore = 15;  // Ninth + tenth: (4 + 5) + (3 + 3)
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+        #endregion
     }
 }
