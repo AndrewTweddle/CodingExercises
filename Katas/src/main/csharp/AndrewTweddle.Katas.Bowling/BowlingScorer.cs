3a6b191abe5ca0c03b85bc047f0f5d9f8aa152eb
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-22 20:19:32 -0400

    Bowling Kata: Empty implementation and failing test
    - Set up the projects for the Bowling Kata from codingdojo.org
    - Use a JVM folder structure for consistent comparison across languages
    in future
    - Create a dummy BowlingScorer returning a value of zero
    - Create a single failing unit test

---
+++ Katas/src/main/csharp/AndrewTweddle.Katas.Bowling/BowlingScorer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrewTweddle.Katas.Bowling
+{
+    public static class BowlingScorer
+    {
+        public static int Calculate(string throwSymbols)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringTheFinalFrame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AndrewTweddle.Katas.Bowling;
+
+namespace AndrewTweddle.Katas.Test.Bowling
+{
+    [TestClass]
+    public class WhenScoringTheFinalFrame
+    {
+        [TestMethod]
+        public void ThenExtraThrowsDontScoreForBeingAStrike()
+        {
+            String throwChars = new string('-', 18) + "xxx";
+            int expectedScore = 30;  // Zeroes for 9 frames, 30 for 10th frame
+            int actualScore = BowlingScorer.Calculate(throwChars);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+    }
+}
