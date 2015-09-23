fc0a422dbb071a6d06ea32014cb6e3128adfd934
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-23 12:48:10 -0400

    Kata: Bowling: Test that all strikes scores 300

---
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringAGame.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AndrewTweddle.Katas.Bowling;
+
+namespace AndrewTweddle.Katas.Test.Bowling
+{
+    [TestClass]
+    public class WhenScoringAGame
+    {
+        [TestMethod]
+        public void ThenAllStrikesScores300()
+        {
+            string symbols = new string('X', 12);
+                // i.e. 10 strikes and 2 bonus throws in the tenth frame
+            int expectedScore = 300;
+            int actualScore = BowlingScorer.Calculate(symbols);
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+    }
+}
