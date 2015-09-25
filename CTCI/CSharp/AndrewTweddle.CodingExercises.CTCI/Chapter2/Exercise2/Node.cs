1848f147fdafda5bbeed4c3e0fc3c5c7fda02f6e
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-24 20:11:55 -0400

    CTCI Ex. 2.2: Initial code to find k'th last node
    - Initial code prior to unit testing for finding the k'th to last node
    in a single linked list.
    - The linked list is implemented in a naive manner as a Node class only
    - An empty linked list is represented as a null Node
    - The Node class is immutable, as is the value it contains (but of
    course properties of the generic type could be changed if the value type
    was mutable).

---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI/Chapter2/Exercise2/Node.cs
@@ -0,0 +1,57 @@
+﻿namespace AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise2
+{
+    /// <summary>
+    /// A crude implementation of a single linked list using a Node class.
+    /// The Node and its Value are both immutable.
+    /// </summary>
+    public class Node<T>
+    {
+        public T Value { get; private set; }
+        public Node<T> NextNode { get; private set; }
+        public Node(T value, Node<T> nextNode)
+        {
+            Value = value;
+            NextNode = nextNode;
+        }
+    }
+
+    public static class NodeHelper
+    {
+        public static bool TrySkip<T>(ref Node<T> node, int numberToSkip)
+        {
+            for (int i = 0; i < numberToSkip; i++)
+            {
+                if (node.NextNode == null) return false;
+                node = node.NextNode;
+            }
+            return true;
+        }
+
+        private static Node<T> FindLastNode<T>(Node<T> startNode, 
+            Node<T> rootNode)
+        {
+            while (startNode.NextNode != null)
+            {
+                startNode = startNode.NextNode;
+                rootNode = rootNode.NextNode;
+            }
+            return rootNode;
+        }
+
+        public static bool TryFindKLastNode<T>(Node<T> rootNode, int k, 
+            out Node<T> kLastNode)
+        {
+            Node<T> endNodeFinder = rootNode;
+            bool canSkipKNodes = TrySkip(ref endNodeFinder, k);
+            if (canSkipKNodes)
+            {
+                kLastNode = FindLastNode(endNodeFinder, rootNode);
+            }
+            else
+            {
+                kLastNode = null;
+            }
+            return canSkipKNodes;
+        }
+    }
+}
