dc7ab11e43613b10aec7ae85ab6571cc72688cdc
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-25 15:35:19 -0400

    Kata: Bowling: New algorithm
    - designed a new algorithm based on expressing problem constraints as
    mathematical symbols
    - wrote BowlingScorer2 to express the insights gained, but giving human
    readable names instead of Mathematical symbols
    - Refactored out smaller methods to describe the intent
    - I still had a monolithic method until I separated concerns by
    splitting out the methods for CalculateFrameScore() and
    GetIndexOfNextFrame()
    - The duplication in these 2 methods feels okay, because it's delegating
    to the smaller descriptive methods
    - I still don't like that BowlingScorer was changed from a static class
    purely for the convenience of sharing unit tests with BowlingScorer2

---
+++ Katas/src/main/csharp/AndrewTweddle.Katas.Bowling/BowlingScorer2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndrewTweddle.Katas.Bowling
+{
+    public class BowlingScorer2 : IBowlingScorer
+    {
+        private const int LAST_FRAME = 10;
+
+        private Dictionary<char, int> charToPins = new Dictionary<char, int>
+        {
+            {'.', 0 },
+            {'1', 1 },
+            {'2', 2 },
+            {'3', 3 },
+            {'4', 4 },
+            {'5', 5 },
+            {'6', 6 },
+            {'7', 7 },
+            {'8', 8 },
+            {'9', 9 },
+            {'X', 10 },
+        };
+
+        public int GetPinsDown(string symbols, int index)
+        {
+            return charToPins[symbols[index]];
+        }
+
+        public int SumOfTwoThrows(string symbols, int firstIndex)
+        {
+            if (symbols[firstIndex + 1] == '/')
+                return 10;  // Spare
+            else
+                return GetPinsDown(symbols, firstIndex)
+                    + GetPinsDown(symbols, firstIndex + 1);
+        }
+
+        public bool IsStrike(string symbols, int indexOfFirstThrowInFrame)
+        {
+            return symbols[indexOfFirstThrowInFrame] == 'X';
+        }
+
+        public bool IsSpare(string symbols, int indexOfFirstThrowInFrame)
+        {
+            return symbols[indexOfFirstThrowInFrame + 1] == '/';
+        }
+
+        public int ScoreFrameWithAStrike(string symbols, 
+            int indexOfFirstThrowInFrame)
+        {
+            return 10 + SumOfTwoThrows(symbols, indexOfFirstThrowInFrame + 1);
+        }
+
+        public int ScoreFrameWithASpare(string symbols,
+            int indexOfFirstThrowInFrame)
+        {
+            return 10 + GetPinsDown(symbols, indexOfFirstThrowInFrame + 2);
+        }
+
+        public int ScoreANormalFrame(string symbols,
+            int indexOfFirstThrowInFrame)
+        {
+            return SumOfTwoThrows(symbols, indexOfFirstThrowInFrame);
+        }
+
+        public int CalculateFrameScore(string symbols,
+            int indexOfFirstThrowInFrame)
+        {
+            if (IsStrike(symbols, indexOfFirstThrowInFrame))
+            {
+                return ScoreFrameWithAStrike(symbols, indexOfFirstThrowInFrame);
+            }
+            if (IsSpare(symbols, indexOfFirstThrowInFrame))
+            {
+                return ScoreFrameWithASpare(symbols, indexOfFirstThrowInFrame);
+            }
+            return ScoreANormalFrame(symbols, indexOfFirstThrowInFrame);
+        }
+
+        public int GetIndexOfNextFrame(string symbols,
+            int indexOfFirstThrowInThisFrame)
+        {
+            if (IsStrike(symbols, indexOfFirstThrowInThisFrame))
+            {
+                return indexOfFirstThrowInThisFrame + 1;
+            }
+            return indexOfFirstThrowInThisFrame + 2;
+        }
+
+        public int Calculate(string symbols)
+        {
+            int score = 0;
+            int throwIndexForFrame = 0;
+            for (int frameNumber = 1; frameNumber <= LAST_FRAME; frameNumber++)
+            {
+                score += CalculateFrameScore(symbols, throwIndexForFrame);
+                throwIndexForFrame 
+                    = GetIndexOfNextFrame(symbols, throwIndexForFrame);
+            }
+            return score;
+        }
+    }
+}
