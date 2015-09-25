33404527b95878c684d2480848311b7f7f2f4588
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-25 15:39:44 -0400

    Kata: Bowling: BowlingScorer2: failing unit tests
    - Add the derived unit test classes for the new BowlingScorer2 algorithm

---
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringAGame2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AndrewTweddle.Katas.Bowling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndrewTweddle.Katas.Test.Bowling
+{
+    [TestClass]
+    public class WhenScoringAGame2: BaseForWhenScoringAGame
+    {
+        protected override IBowlingScorer GetScorer()
+        {
+            return new BowlingScorer2();
+        }
+    }
+}
---
+++ Katas/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringTheFinalFrame2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AndrewTweddle.Katas.Bowling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndrewTweddle.Katas.Test.Bowling
+{
+    [TestClass]
+    public class WhenScoringTheFinalFrame2: BaseForWhenScoringTheFinalFrame
+    {
+        protected override IBowlingScorer GetScorer()
+        {
+            return new BowlingScorer2();
+        }
+    }
+}
