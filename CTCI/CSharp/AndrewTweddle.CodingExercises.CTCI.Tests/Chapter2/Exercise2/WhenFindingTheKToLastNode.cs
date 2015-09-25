6539ca2d62088f422b1fdf4690b389f0722df320
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-24 20:16:01 -0400

    CTCI Ex. 2.2: 1st unit test to find k'th last node
    - a unit test that checks what happens when an empty linked list (null
    root node) is passed to the NodeHelper.TryFindKLastNode(...)
    - the unit test fails, so the code still needs to be fixed
    - Is this a symptom of the decision to implement the linked list as a
    single Node class, rather than a LinkedList class (which can represent
    the empty list) and a Node class?
    - Is this a symptom of the fragility of nulls

---
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter2/Exercise2/WhenFindingTheKToLastNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise2;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Tests.Chapter2.Exercise2
+{
+    [TestClass]
+    public class WhenFindingTheKToLastNode
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ThenPassingANullRootNodeThrowsASensibleException()
+        {
+            Node<int> kLastNode;
+            NodeHelper.TryFindKLastNode(null, 1, out kLastNode);
+        }
+    }
+}
