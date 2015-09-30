25f141f19f723a71ec10075380f75fb64e0bd531
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-09-29 22:43:53 -0400

    CTCI: Ex. 2.2: Get Kth to last node in linked list
    - Crude implementation of linked lists using just a Node class
    (uses a null node to signify an empty linked list, which is ugly)
    - NodeHelper class to get kth to last node
    - Unit tests

--- CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter2/Exercise2/WhenFindingTheKToLastNode.cs
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter2/Exercise2/WhenFindingTheKToLastNode.cs
@@ -8,10 +8,41 @@ namespace AndrewTweddle.CodingExercises.CTCI.Tests.Chapter2.Exercise2
     public class WhenFindingTheKToLastNode
     {
         [TestMethod, ExpectedException(typeof(ArgumentNullException))]
-        public void ThenPassingANullRootNodeThrowsASensibleException()
+        public void ThenPassingANullRootNodeThrowsASensibleExceptionIfKIsNonZero()
         {
-            Node<int> kLastNode;
-            NodeHelper.TryFindKLastNode(null, 1, out kLastNode);
+            Node<int> kLastNode = NodeHelper.FindKLastNode<int>(null, 1);
+        }
+
+        [TestMethod]
+        public void ThenPassingANullRootNodeReturnsNullIfKIsZero()
+        {
+            Node<int> kLastNode = NodeHelper.FindKLastNode<int>(null, 0);
+            Assert.IsNull(kLastNode, 
+                "The zeroth to last node should always be null");
+        }
+
+        [TestMethod]
+        public void ThenTheKthLastNodeWhenThereAreKNodesIsTheRootNode()
+        {
+            Node<int> rootNode = new Node<int>(1, 2, 3);
+            Node<int> kLastNode = NodeHelper.FindKLastNode(rootNode, 3);
+            Assert.AreEqual(rootNode, kLastNode);
+        }
+
+        [TestMethod]
+        public void ThenTheKthLastNodeWhenThereAreKPlus1NodesIsTheSecondNode()
+        {
+            Node<int> rootNode = new Node<int>(1, 2, 3, 4);
+            Node<int> kLastNode = NodeHelper.FindKLastNode(rootNode, 3);
+            Assert.AreEqual(kLastNode.Value, 2);
+        }
+
+        [TestMethod]
+        public void ThenTheFirstToLastNodeIsTheLastNode()
+        {
+            Node<int> rootNode = new Node<int>(1, 2, 3);
+            Node<int> kLastNode = NodeHelper.FindKLastNode(rootNode, 1);
+            Assert.IsNull(kLastNode.NextNode);
         }
     }
 }
--- CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI/Chapter2/Exercise2/Node.cs
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI/Chapter2/Exercise2/Node.cs
@@ -1,4 +1,7 @@
-﻿namespace AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise2
+﻿using System;
+using System.Collections.Generic;
+
+namespace AndrewTweddle.CodingExercises.CTCI.Chapter2.Exercise2
 {
     /// <summary>
     /// A crude implementation of a single linked list using a Node class.
@@ -13,6 +16,26 @@
             Value = value;
             NextNode = nextNode;
         }
+
+        public Node(params T[] values)
+        {
+            if (values.Length == 0)
+            {
+                /* This is a kludge because there is no way to 
+                 * represent an empty root node, except as a null reference
+                 */
+                throw new InvalidOperationException(
+                    "No values provided to Node constructor");
+            }
+            Node<T> lastNode = null;
+            for (int i =  values.Length - 1; i > 0; i--)
+            {
+                T value = values[i];
+                lastNode = new Node<T>(value, lastNode);
+            }
+            Value = values[0];
+            NextNode = lastNode;
+        }
     }
 
     public static class NodeHelper
@@ -21,16 +44,19 @@
         {
             for (int i = 0; i < numberToSkip; i++)
             {
-                if (node.NextNode == null) return false;
+                if (node == null)
+                {
+                    return false;
+                }
                 node = node.NextNode;
             }
             return true;
         }
 
-        private static Node<T> FindLastNode<T>(Node<T> startNode, 
+        private static Node<T> AdvanceToEnd<T>(Node<T> startNode, 
             Node<T> rootNode)
         {
-            while (startNode.NextNode != null)
+            while (startNode != null)
             {
                 startNode = startNode.NextNode;
                 rootNode = rootNode.NextNode;
@@ -38,20 +64,21 @@
             return rootNode;
         }
 
-        public static bool TryFindKLastNode<T>(Node<T> rootNode, int k, 
-            out Node<T> kLastNode)
+        public static Node<T> FindKLastNode<T>(Node<T> rootNode, int k)
         {
+            Node<T> kLastNode = null;
             Node<T> endNodeFinder = rootNode;
             bool canSkipKNodes = TrySkip(ref endNodeFinder, k);
-            if (canSkipKNodes)
+            if (!canSkipKNodes)
             {
-                kLastNode = FindLastNode(endNodeFinder, rootNode);
+                if (rootNode == null)
+                {
+                    throw new ArgumentNullException("rootNode");
+                }
+                throw new ArgumentOutOfRangeException("k");
             }
-            else
-            {
-                kLastNode = null;
-            }
-            return canSkipKNodes;
+            kLastNode = AdvanceToEnd(endNodeFinder, rootNode);
+            return kLastNode;
         }
     }
 }
