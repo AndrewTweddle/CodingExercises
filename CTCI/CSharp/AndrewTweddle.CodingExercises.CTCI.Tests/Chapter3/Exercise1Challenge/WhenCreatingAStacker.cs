ecd3ffdb77e71f157b2f1d9ca2d35cdb623f11e0
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-11-03 11:19:12 -0500

    CTCI Ex. 3.1 challenge: WhenCreatingAStacker tests

--- CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter3/Exercise1Challenge/WhenCreatingAStacker.cs
+++ CTCI/CSharp/AndrewTweddle.CodingExercises.CTCI.Tests/Chapter3/Exercise1Challenge/WhenCreatingAStacker.cs
@@ -24,8 +24,17 @@ namespace AndrewTweddle.CodingExercises.CTCI.Tests.Chapter3.Exercise1Challenge
             for (int i = 0; i < Stacker<int>.STACK_COUNT; i++)
             {
                 Stack<int> stack = stacker[i];
-                Assert.AreEqual(i * EXPECTED_STACK_OFFSET, stack.NextInsertPos);
+                Assert.AreEqual(i * EXPECTED_STACK_OFFSET, stack.FirstInsertPos);
             }
+        }
+
+        [TestMethod]
+        public void ThenAnEmptyStackHasTheSameFirstAndNextInsertPositions()
+        {
+            stacker[0].Push(1);
+
+            Assert.AreNotEqual(stacker[0].FirstInsertPos, stacker[0].NextInsertPos);
+            Assert.AreEqual(stacker[1].FirstInsertPos, stacker[1].NextInsertPos);
         }
     }
 }
