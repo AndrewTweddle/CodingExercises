7aca336d33ece8243ca1344882345a839d91c6be
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2020-01-25 09:43:03 -0500

    C++ prototype for rectangle of words problem
    Rewrite Scala algorithm in C++
    
    - This doesn't yet have the code to search through all grid sizes,
      from largest to smallest
    - Instead I hard-coded specific grid dimensions
    - This was so that I could easily compare the performance
      of the C++ and Scala code on grid sizes where
      I already had performance data from the Scala run,
      and which I knew wouldn't run for hours
    - I was surprised to see that performance was 2.5 to 3 times worse
    - An algorithm change in the C++ code (vs Scala)
      is to iterate through the child lists of both the row and column tries
      to find characters that are in both,
      instead of using a dictionary lookup (as I did in the Scala prototype)
    - Due to that algorithm change, I also didn't think it necessary
      to treat the first column differently from subsequent columns,
      as I did with the Scala prototype.

---
+++ misc/WordRectangles/C++/main.cpp
@@ -0,0 +1,322 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <chrono>
+
+using std::map;
+using std::vector;
+using std::string;
+using std::pair;
+using std::cout;
+using std::endl;
+using Clock = std::chrono::steady_clock;
+using std::chrono::time_point;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+
+class TrieNode
+{
+  public:
+    TrieNode(): mWordCount(0) {};
+    map<char, TrieNode>::const_iterator getIter() const { return mSubNodes.cbegin(); }
+    bool hasMoreChars(map<char, TrieNode>::const_iterator & iter) const { iter != mSubNodes.cend(); }
+    bool tryToAddString(const string & word);
+  private:
+    bool tryToAddSubString(const string & word, size_t startPos, size_t maxStartPos);
+    
+    map<char, TrieNode> mSubNodes;
+    size_t mWordCount;
+};
+
+bool TrieNode::tryToAddString(const string & word)
+{
+    size_t maxStartPos = word.length() - 1;
+    return tryToAddSubString(word, 0, maxStartPos);
+}
+
+bool TrieNode::tryToAddSubString(const string & word, size_t startPos, size_t maxStartPos)
+{
+    char nextChar = word[startPos];
+    
+    // If it's the first char in this branch of the trie, then it's not a duplicate
+    auto subNodeIter = mSubNodes.find(nextChar);
+    bool isNotADuplicate = subNodeIter == mSubNodes.end();
+    
+    // If we're at the last level a dummy trie node will be created, but won't be used
+    if (startPos < maxStartPos)
+    {
+        isNotADuplicate = mSubNodes[nextChar].tryToAddSubString(word, startPos+1, maxStartPos);
+    }
+    else
+    {
+        if (isNotADuplicate)
+        {
+            // Create the new leaf-level sub-node
+            mSubNodes[nextChar].mWordCount = 1;
+        }
+    }
+    
+    if (isNotADuplicate)
+    {
+        mWordCount++;
+        return true;
+    }
+    
+    // Duplicate word found
+    return false;
+}
+
+struct GridSolver
+{
+    GridSolver(size_t initRowCount, size_t initColCount, map<size_t, TrieNode> & trieByLength);
+    bool solveFirstColumn();
+    bool solveColumn(size_t colId, const vector<const TrieNode *> & rowTriesInPrevCol);
+    
+    const TrieNode & rootRowTrie;
+    const TrieNode & rootColTrie;
+    const size_t rowCount;
+    const size_t colCount;
+    vector<vector<char>> solution;
+};
+
+struct ColumnSolver
+{
+    ColumnSolver(GridSolver & initGridSolver, size_t initColId, const vector<const TrieNode *> & initRowTriesInPrevCol)
+            : gridSolver(initGridSolver),
+              colId(initColId),
+              rowTriesInPrevCol(initRowTriesInPrevCol)
+    {};
+    
+    bool solveCell(vector<const TrieNode *> & rowTriesInCurrCol, size_t rowId, const TrieNode & currColTrie);
+    
+    GridSolver & gridSolver;
+    size_t colId;
+    const vector<const TrieNode *> & rowTriesInPrevCol;
+};
+
+void populateMapOfWordsByLength(const string &inputFilePath, map<size_t, vector<string>> & wordsByLength);
+void populateMapOfTriesByLength(
+        const map<size_t, vector<string>> & wordsByLength, map<size_t, TrieNode> & trieByLength);
+void solve(map<size_t, TrieNode> & trieByLength);
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1 || argv[1] == "--help")
+    {
+        cout << "wordrect PathToFileWithAWordPerLine\n";
+        cout << "The input file path is an ASCII text file with words on each line." << endl;
+        return 0;
+    }
+    
+    time_point<Clock> start = Clock::now();
+    
+    string inputFilePath(argv[1]);
+    map<size_t, vector<string> > wordsByLength;
+    populateMapOfWordsByLength(inputFilePath,wordsByLength);
+    
+    map<size_t, TrieNode> trieByLength;
+    populateMapOfTriesByLength(wordsByLength, trieByLength);
+    
+    solve(trieByLength);
+    
+    time_point<Clock> end = Clock::now();
+    milliseconds duration = duration_cast<milliseconds>(end - start);
+    cout << "\nTOTAL DURATION: " << duration.count() << " ms\n";
+    
+    return 0;
+}
+
+void populateMapOfWordsByLength(
+        const string &inputFilePath,
+        map<size_t, vector<string>> & wordsByLength)
+{
+    cout << "Reading words from input file (grouping by word length)..." << endl << endl;
+    time_point<Clock> start = Clock::now();
+
+    std::ifstream in(inputFilePath);
+    
+    string word;
+    while (in >> word)
+    {
+        wordsByLength[word.length()].push_back(word);
+    }
+    in.close();
+    
+    cout << "Word counts by length:" << endl;
+    cout << "======================" << endl;
+    for (auto lenIter = wordsByLength.cbegin(); lenIter != wordsByLength.cend(); ++lenIter)
+    {
+        auto lenWordsMap = *lenIter;
+        cout << lenWordsMap.first << ": " << lenWordsMap.second.size() << endl;
+    }
+    cout << endl;
+    
+    time_point<Clock> end = Clock::now();
+    milliseconds duration = duration_cast<milliseconds>(end - start);
+    cout << "Duration: " << duration.count() << " ms\n" << endl;
+}
+
+void populateMapOfTriesByLength(
+        const map<size_t, vector<string>> &wordsByLength,
+        map<size_t, TrieNode> & trieByLength)
+{
+    cout << "Reading words into tries (by word length)..." << endl << endl;
+    time_point<Clock> start = Clock::now();
+    
+    size_t uniqueWordCount = 0;
+    for (auto lenToWords: wordsByLength)
+    {
+        auto & trie = trieByLength[lenToWords.first];
+        for (const auto& wordIter: lenToWords.second)
+        {
+            if (trie.tryToAddString(wordIter))
+            {
+                uniqueWordCount++;
+            }
+        }
+    }
+    
+    time_point<Clock> end = Clock::now();
+    milliseconds duration = duration_cast<milliseconds>(end - start);
+    cout << "Duration: " << duration.count() << " ms\n";
+    cout << "Total unique words: " << uniqueWordCount << endl << endl;
+}
+
+bool tryToSolveGrid(size_t rowCount, size_t colCount, map<size_t, TrieNode> & trieByLength);
+
+void solve(map<size_t, TrieNode> & trieByLength)
+{
+    // Test with a grid size that took around 20 seconds in the Scala solution
+    size_t rowCount = 7;
+    size_t colCount = 18;
+    
+    // Try something much harder, to look for bugs
+    rowCount = 14;
+    colCount = 14;
+    tryToSolveGrid(rowCount, colCount, trieByLength);
+}
+
+bool tryToSolveGrid(size_t rowCount, size_t colCount, map<size_t, TrieNode> & trieByLength)
+{
+    cout << "    " << rowCount << " x " << colCount << endl;
+    time_point<Clock> start = Clock::now();
+    
+    GridSolver gridSolver(rowCount, colCount, trieByLength);
+    bool isSolved = gridSolver.solveFirstColumn();
+
+    if (!isSolved)
+    {
+        cout << "        0 solutions found\n";
+    }
+    
+    time_point<Clock> end = Clock::now();
+    milliseconds duration = duration_cast<milliseconds>(end - start);
+    cout << "        Search Duration: " << duration.count() << " ms" << endl;
+    
+    return isSolved;
+}
+
+GridSolver::GridSolver(size_t initRowCount, size_t initColCount, map<size_t, TrieNode> & trieByLength)
+    : rootRowTrie(trieByLength[initRowCount]),
+      rootColTrie(trieByLength[initColCount]),
+      rowCount(initRowCount),
+      colCount(initColCount)
+{
+    solution.resize(initRowCount);
+    for (auto &row : solution)
+    {
+        row.resize(initColCount);
+    }
+}
+
+bool GridSolver::solveColumn(size_t colId, const vector<const TrieNode *> & rowTriesInPrevCol)
+{
+    if (colId == colCount)
+    {
+        // A solution was found
+        cout << "        solution found:\n";
+        for (const auto &row: solution)
+        {
+            for (const auto nextChar: row)
+            {
+                cout << nextChar;
+            }
+            cout << endl;
+        }
+        return true;
+    }
+    
+    vector<const TrieNode *> rowTriesInCurrCol(rowCount);
+    ColumnSolver nextColSolver(*this, colId, rowTriesInPrevCol);
+    
+    return nextColSolver.solveCell(rowTriesInCurrCol, 0, rootColTrie);
+}
+
+bool GridSolver::solveFirstColumn()
+{
+    vector<const TrieNode *> rootRowTriesForEachRow(rowCount, &rootRowTrie);
+    return solveColumn(0, rootRowTriesForEachRow);
+}
+
+bool ColumnSolver::solveCell(vector<const TrieNode *> & rowTriesInCurrCol, size_t rowId, const TrieNode & currColTrie)
+{
+    const TrieNode & currRowTrie = *rowTriesInPrevCol[rowId];
+    auto rowCharIter = currRowTrie.getIter();
+    auto colCharIter = currColTrie.getIter();
+    bool rowCharsNotDone = currRowTrie.hasMoreChars(rowCharIter);
+    bool colCharsNotDone = currColTrie.hasMoreChars(colCharIter);
+    while (rowCharsNotDone && colCharsNotDone)
+    {
+        char colChar = colCharIter->first;
+        while (rowCharsNotDone && rowCharIter->first < colChar)
+        {
+            ++rowCharIter;
+            rowCharsNotDone = currRowTrie.hasMoreChars(rowCharIter);
+        }
+        if (rowCharsNotDone)
+        {
+            char rowChar = rowCharIter->first;
+            if (rowChar > colChar)
+            {
+                while (colCharsNotDone && rowChar > colCharIter->first)
+                {
+                    ++colCharIter;
+                    colCharsNotDone = currColTrie.hasMoreChars(colCharIter);
+                }
+                if (colCharsNotDone)
+                {
+                    colChar = colCharIter->first;
+                }
+            }
+            
+            if (colCharsNotDone && rowChar == colChar)
+            {
+                gridSolver.solution[rowId][colId] = rowChar;
+                rowTriesInCurrCol[rowId] = &rowCharIter->second;
+                if (rowId == gridSolver.rowCount - 1)
+                {
+                    // This column is complete. Solve the next column.
+                    if (gridSolver.solveColumn(colId + 1, rowTriesInCurrCol))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    // Find solutions for next cell (one row down in same column)
+                    const TrieNode & newCurrColTrieNode = colCharIter->second;
+                    if (solveCell(rowTriesInCurrCol, rowId + 1, newCurrColTrieNode))
+                    {
+                        return true;
+                    }
+                }
+                rowCharIter++;
+                rowCharsNotDone = currRowTrie.hasMoreChars(rowCharIter);
+                colCharIter++;
+                colCharsNotDone = currColTrie.hasMoreChars(colCharIter);
+            }
+        }
+    }
+    return false;
+}
