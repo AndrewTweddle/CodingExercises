cb58636c9f7a3dee7bfe5e4f1c2131d36b627b2b
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2021-05-24 15:49:26 -0400

    Fix invalid Roman numerals like "IVI" being converted without an error
    - I already had this solution in mind before adding the unit tests and property-based checks to the project
    - I wanted to see if the tests would catch the issue
    - The unit tests didn't, but one of the property-based tests did

--- Katas/CodingDojo/RomanNumerals/Rust/roman_numerals_v2/src/main.rs
+++ Katas/CodingDojo/RomanNumerals/Rust/roman_numerals_v2/src/main.rs
@@ -24,33 +24,37 @@ fn main() {
     println!("Duration per number: {:?}", duration / 3000);
 
     let edge_case = "VIV";
-    let value_of_edge_case = convert_from_roman(edge_case).unwrap();
-    println!("Validation issue: conversion thinks that {} = {} (for example)",
-             edge_case, value_of_edge_case);
-
+    let result_of_edge_case = convert_from_roman(edge_case);
+    match result_of_edge_case {
+        Ok(value_of_edge_case) =>
+            println!("Validation issue: conversion thinks that {} = {} (for example)",
+                     edge_case, value_of_edge_case),
+        Err(e) => println!("As expected, an error occurred processing {}: {}", edge_case, e),
+    }
     println!("is_roman(\"{}\") = {}", edge_case, is_roman(edge_case));
 }
 
 struct Pattern {
     pattern: &'static str,
     value: u16,
-    max_repetitions: usize
+    max_repetitions: usize,
+    steps_to_skip: usize,
 }
 
 const PATTERNS: [Pattern; 13] = [
-    Pattern{ pattern: "M",  value: 1000, max_repetitions: 3 },
-    Pattern{ pattern: "CM", value:  900, max_repetitions: 1 },
-    Pattern{ pattern: "D",  value:  500, max_repetitions: 1 },
-    Pattern{ pattern: "CD", value:  400, max_repetitions: 1 },
-    Pattern{ pattern: "C",  value:  100, max_repetitions: 3 },
-    Pattern{ pattern: "XC", value:   90, max_repetitions: 1 },
-    Pattern{ pattern: "L",  value:   50, max_repetitions: 1 },
-    Pattern{ pattern: "XL", value:   40, max_repetitions: 1 },
-    Pattern{ pattern: "X",  value:   10, max_repetitions: 3 },
-    Pattern{ pattern: "IX", value:    9, max_repetitions: 1 },
-    Pattern{ pattern: "V",  value:    5, max_repetitions: 1 },
-    Pattern{ pattern: "IV", value:    4, max_repetitions: 1 },
-    Pattern{ pattern: "I",  value:    1, max_repetitions: 3 },
+    Pattern{ pattern: "M",  value: 1000, max_repetitions: 3, steps_to_skip: 0 },
+    Pattern{ pattern: "CM", value:  900, max_repetitions: 1, steps_to_skip: 3 },
+    Pattern{ pattern: "D",  value:  500, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "CD", value:  400, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "C",  value:  100, max_repetitions: 3, steps_to_skip: 0 },
+    Pattern{ pattern: "XC", value:   90, max_repetitions: 1, steps_to_skip: 3 },
+    Pattern{ pattern: "L",  value:   50, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "XL", value:   40, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "X",  value:   10, max_repetitions: 3, steps_to_skip: 0 },
+    Pattern{ pattern: "IX", value:    9, max_repetitions: 1, steps_to_skip: 3 },
+    Pattern{ pattern: "V",  value:    5, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "IV", value:    4, max_repetitions: 1, steps_to_skip: 1 },
+    Pattern{ pattern: "I",  value:    1, max_repetitions: 3, steps_to_skip: 0 },
 ];
 
 pub fn convert_to_roman(mut num: u16) -> Result<String, &'static str> {
@@ -76,17 +80,24 @@ pub fn convert_from_roman(roman: &str) -> Result<u16, &'static str> {
     }
     let mut num = 0;
     let mut start_pos = 0;
-    'pat_loop: for pat in &PATTERNS {
+    let mut pat_index = 0;
+    'pat_loop: while pat_index < PATTERNS.len() {
+        let pat = &PATTERNS[pat_index];
+        let mut pattern_matched = false;
         for _ in 0..pat.max_repetitions {
             let rem_str = &roman[start_pos..];
             if rem_str.starts_with(pat.pattern) {
                 num += pat.value;
                 start_pos += &pat.pattern.len();
                 if start_pos == roman.len() { break 'pat_loop; }
+                pattern_matched = true;
             } else {
                 break;
             }
         }
+        // When certain patterns are matched,other patterns should be skipped over
+        // e.g. if "XC" is matched, then "L", "XL" and "X" should all be skipped.
+        pat_index += if pattern_matched { pat.steps_to_skip + 1 } else { 1 }
     }
     if start_pos == roman.len() {
         if num > 3000 {
