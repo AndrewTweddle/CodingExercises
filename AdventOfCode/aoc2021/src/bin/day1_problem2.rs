c061a7559a41e3b07d06d8879bbc5c1e2b54867d
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2021-12-01 22:13:17 -0500

    Solve AOC 2021 day 1 part 2
    - Also fix "Date Solved" in readme.md for day 1 part 1

---
+++ AdventOfCode/aoc2021/src/bin/day1_problem2.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let path = "data/day1_input";
+    let input = File::open(path).unwrap();
+    let br = BufReader::new(input);
+    let depths = br
+        .lines()
+        .map(|ln| ln.unwrap().parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    let sliding_window_sums = depths
+        .windows(3)
+        .map(|sliding_window| sliding_window
+            .iter()
+            .map(|&depth| depth)
+            .sum::<u32>());
+    let increasing_count = sliding_window_sums
+        .clone()
+        .zip(sliding_window_sums.skip(1))
+        .filter(|&(sum1, sum2)| sum2 > sum1)
+        .count();
+    println!("Number of sliding windows that increased: {}", increasing_count);
+}
