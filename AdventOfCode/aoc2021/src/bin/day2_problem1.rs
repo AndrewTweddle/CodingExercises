a78cfa50c7267bd9c243139f4021dbf69000a9d8
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2021-12-02 20:43:08 -0500

    Solve AOC 2021 day 2 part 1

---
+++ AdventOfCode/aoc2021/src/bin/day2_problem1.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let path = "data/day2_input";
+    let input_file = File::open(path).unwrap();
+    let br = BufReader::new(input_file);
+    let (final_x, final_y) = br
+        .lines()
+        .fold((0_i32, 0_i32), |(x, y), ln| {
+            let line = ln.unwrap();
+            let mut split_line = line.split(' ');
+            let direction = split_line.next().unwrap();
+            let distance = split_line.next().unwrap().parse::<i32>().unwrap();
+            match direction {
+                "forward" => (x+distance, y),
+                "up" => (x, y-distance),
+                "down" => (x, y+distance),
+                _ => panic!("Unrecognized instruction: {}", direction),
+            }
+        });
+    println!("Final position: ({}, {})", final_x, final_y);
+    println!("Product: {}", final_x * final_y);
+}
