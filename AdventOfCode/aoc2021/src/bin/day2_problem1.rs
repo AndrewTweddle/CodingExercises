c12f878c1e7aa2201a7d8f9d2daf9717fb26ae42
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2021-12-02 21:29:13 -0500

    Reformat AOC 2021 solutions using Rustfmt
    - And rename a variable to make it clear that it is an iterator

--- AdventOfCode/aoc2021/src/bin/day2_problem1.rs
+++ AdventOfCode/aoc2021/src/bin/day2_problem1.rs
@@ -5,20 +5,18 @@ fn main() {
     let path = "data/day2_input";
     let input_file = File::open(path).unwrap();
     let br = BufReader::new(input_file);
-    let (final_x, final_y) = br
-        .lines()
-        .fold((0_i32, 0_i32), |(x, y), ln| {
-            let line = ln.unwrap();
-            let mut split_line = line.split(' ');
-            let direction = split_line.next().unwrap();
-            let distance = split_line.next().unwrap().parse::<i32>().unwrap();
-            match direction {
-                "forward" => (x+distance, y),
-                "up" => (x, y-distance),
-                "down" => (x, y+distance),
-                _ => panic!("Unrecognized instruction: {}", direction),
-            }
-        });
+    let (final_x, final_y) = br.lines().fold((0_i32, 0_i32), |(x, y), ln| {
+        let line = ln.unwrap();
+        let mut line_fields_iter = line.split(' ');
+        let direction = line_fields_iter.next().unwrap();
+        let distance = line_fields_iter.next().unwrap().parse::<i32>().unwrap();
+        match direction {
+            "forward" => (x + distance, y),
+            "up" => (x, y - distance),
+            "down" => (x, y + distance),
+            _ => panic!("Unrecognized instruction: {}", direction),
+        }
+    });
     println!("Final position: ({}, {})", final_x, final_y);
     println!("Product: {}", final_x * final_y);
 }
