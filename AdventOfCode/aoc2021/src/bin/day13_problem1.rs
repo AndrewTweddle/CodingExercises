bc3ba8a024718ce0b1bfdd64a6f03ab95a5046ab
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2021-12-13 21:11:35 -0500

    Cosmetic changes to AOC 2021 day 13 part 1 and 2

--- AdventOfCode/aoc2021/src/bin/day13_problem1.rs
+++ AdventOfCode/aoc2021/src/bin/day13_problem1.rs
@@ -36,6 +36,7 @@ fn parse_inputs(contents: &str) -> (Vec<Pos>, Vec<Instruction>) {
         .find(|(_, line)| line.is_empty())
         .unwrap()
         .0;
+
     let coordinates: Vec<Pos> = contents
         .lines()
         .take(blank_line_index)
