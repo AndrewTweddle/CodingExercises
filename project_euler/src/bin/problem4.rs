6e2795d5e8cd9eca2c2f59e6b065a750f0cd1847
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-01-01 20:22:38 -0500

    Solve Project Euler problem 4
    - Actually solved in April 2021
    - Copied from a private repo into the CodingExercises repo

---
+++ project_euler/src/bin/problem4.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut max_palindrome = 0;
+    for i in (100..999).rev() {
+        'smaller: for j in (100..=i).rev() {
+            let prod = i * j;
+            let digits = get_rev_digits(prod);
+            let num_digits = digits.len();
+            for k in 0..(num_digits / 2) {
+                if digits[k] != digits[num_digits - k - 1] {
+                    continue 'smaller;
+                }
+            }
+            max_palindrome = max_palindrome.max(prod);
+        }
+    }
+    println!("Max palindrome = {}", max_palindrome);
+}
+
+// Assume n > 0...
+fn get_rev_digits(n: i64) -> Vec<i64> {
+    let mut rev_digits = Vec::<i64>::new();
+    let mut quot = n;
+    while quot > 0 {
+        let rem = quot % 10;
+        rev_digits.push(rem);
+        quot = quot / 10;
+    }
+    rev_digits
+}
