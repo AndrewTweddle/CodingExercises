3801a51dfaabf3637c634acc4e37d42c35641c40
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-01-01 20:24:28 -0500

    Solve Project Euler problem 5
    - Actually solved in April 2021
    - Copied from a private repo into the CodingExercises repo

---
+++ project_euler/src/bin/problem5.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let smallest_multiple = (2..=20).reduce(|acc, i| lcm(acc, i)).unwrap();
+    println!("Smallest multiple of 2 to 20 is {}", smallest_multiple)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if a < b {
+        gcd(b, a)
+    } else {
+        let rem = a % b;
+        if rem == 0 {
+            b
+        } else {
+            gcd(b, rem)
+        }
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
