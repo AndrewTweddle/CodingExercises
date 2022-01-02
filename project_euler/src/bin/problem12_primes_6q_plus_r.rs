0aa18f1d49440a5d5c3e392f9fd398b4b1fd3c96
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-01-02 13:05:36 -0500

    Solve Project Euler problem 12: alternative 6
    - Alternative 6: As with alternative 5, but after reducing the number
      by primes up to 31, count divisors of the remaining number by
      only considering divisors of the form 6q+1 or 6q+5.
    - Note that the performance testing now uses 100 repetitions not 1.
      This has also been adjusted for the other fast alternatives.
      The performance numbers have been re-run and the readme.md updated.
    - Actually solved in September 2021
    - Copied from a private repo into the CodingExercises repo

--- project_euler/src/bin/problem12_primes_6q_plus_r.rs
+++ project_euler/src/bin/problem12_primes_6q_plus_r.rs
@@ -2,29 +2,29 @@ use std::time::Instant;
 
 const NUM_REPETITIONS: u32 = 100;
 const NUM_DIVISORS: u64 = 500;
-const PRIME_CUTOFF: u64 = 30;
+const PRIME_CUTOFF: u64 = 36;
+const PRIMES: [u64; 11] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+// NUM_DIVISORS    RELEASE MODE     SOLUTION
+//          500        1.423088ms      T(12375) =        76576500
+//         2000       65.347673ms     T(313599) =     49172323200
+//         5000      891.093034ms    T(2203200) =   2427046221600
+//        10000   13.087536584s     T(14753024) = 108825865948800
 
 fn main() {
     let start = Instant::now();
     for rep in 0..NUM_REPETITIONS {
-        let primes = get_primes_up_to(PRIME_CUTOFF);
-        if rep == 0 {
-            println!("# primes up to {} = {}", PRIME_CUTOFF, primes.len());
-        }
-
         let mut triangular_num: u64 = 0;
         let mut divs_i = 1;
-
         for i in 1_u64.. {
             triangular_num += i;
             let divs_i_plus_1 = if i % 2 == 0 {
-                count_divisors_using_prime_decomposition(i + 1, &primes)
+                count_divisors_using_prime_decomposition(i + 1)
             } else {
                 // if i+1 is even, count divisors of (i+1)/2
-                count_divisors_using_prime_decomposition((i + 1) / 2, &primes)
+                count_divisors_using_prime_decomposition((i + 1) / 2)
             };
             // i and i+1 have no common factors except 1
-            // (since common factors divide their difference, and the difference is 1).
+            // (since common factors divide the difference, and the difference is 1).
             // So after first halving whichever of i and i + 1 is even, we can multiply
             // the number of divisors of each to get the number of divisors of their product
             if divs_i * divs_i_plus_1 > NUM_DIVISORS {
@@ -46,60 +46,53 @@ fn main() {
     println!("Avg duration: {:?}", duration / NUM_REPETITIONS);
 }
 
-fn get_primes_up_to(n: u64) -> Vec<u64> {
-    let mut is_prime = vec![true; (n + 1) as usize];
-    is_prime[0] = false;
-    is_prime[1] = false;
-    for candidate_prime in 2..=n {
-        if !is_prime[candidate_prime as usize] {
-            continue;
-        }
-        let mut curr_multiple = candidate_prime * 2;
-        while curr_multiple <= n {
-            is_prime[curr_multiple as usize] = false;
-            curr_multiple += candidate_prime;
-        }
-    }
-
-    is_prime
-        .iter()
-        .enumerate()
-        .filter(|i_prime| *i_prime.1)
-        .map(|i_prime| i_prime.0 as u64)
-        .collect()
-}
-
-fn count_divisors_using_prime_decomposition(mut n: u64, primes: &Vec<u64>) -> u64 {
+fn count_divisors_using_prime_decomposition(mut n: u64) -> u64 {
     let mut div_count = 1;
-    for &prime in primes {
+    for prime in PRIMES {
         let mut prime_exponent = 0;
         while n % prime == 0 {
             n /= prime;
             prime_exponent += 1;
         }
-        // prime^prime_count has prime_count+1 divisors (1, prime, prime^2, ..., prime^prime_count)
+        // prime^prime_count has divisors: 1, prime, prime^2, ..., prime^prime_count
         let prime_power_divs = prime_exponent + 1;
-        div_count *= prime_power_divs
+        div_count *= prime_power_divs;
+
+        // Surprisingly, the following line doubles run-time...
+        // if n == 1 { break; }
     }
     if n != 1 {
-        let rem_divs = count_divisors_without_primes(n);
+        let rem_divs = count_divs_excl_small_primes(n);
         div_count *= rem_divs;
     }
     div_count
 }
 
-fn count_divisors_without_primes(n: u64) -> u64 {
+fn count_divs_excl_small_primes(n: u64) -> u64 {
     let floor_sqrt_n = int_sqrt(n);
 
     // All numbers up to PRIME_CUTOFF are decomposed into powers of the selected primes,
-    // so none of them will be divisors of n, since n contains none of these primes as factors
-    // EXCEPT for 1.
-    let div_pair_count = ((PRIME_CUTOFF + 1)..=floor_sqrt_n)
-        .filter(|i| i % 2 != 0 && n % i == 0)
-        .count() as u64
-        + 1;
+    // so none of them will be divisors of n, since n contains none of these primes as factors.
+    // The exception is 1...
+    let mut div_pair_count = 1;
+    let start_num = PRIME_CUTOFF + 1;
+
+    // count divisors of the form 6q+1...
+    let start_index_1 = start_num / 6 + if start_num % 6 > 1 { 1 } else { 0 };
+    let end_index_1 = floor_sqrt_n / 6 - if floor_sqrt_n % 6 < 1 { 1 } else { 0 };
+    div_pair_count += (start_index_1..=end_index_1)
+        .filter(|q| n % (6 * q + 1) == 0)
+        .count() as u64;
+
+    // count divisors of the form 6q+5...
+    let start_index_1 = start_num / 6 + if start_num % 6 > 5 { 1 } else { 0 };
+    let end_index_1 = floor_sqrt_n / 6 - if floor_sqrt_n % 6 < 5 { 1 } else { 0 };
+    div_pair_count += (start_index_1..=end_index_1)
+        .filter(|q| n % (6 * q + 5) == 0)
+        .count() as u64;
+
     if floor_sqrt_n * floor_sqrt_n == n {
-        // Don't double count the int_sqrt divisor...
+        // Don't double count the floor_sqrt_n divisor...
         div_pair_count * 2 - 1
     } else {
         div_pair_count * 2
