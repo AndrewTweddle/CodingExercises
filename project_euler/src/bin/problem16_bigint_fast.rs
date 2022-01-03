b6fb8f219647440a22e3039166826d7b1dc91ece
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-01-02 22:45:18 -0500

    Solve Project Euler problem 16: attempt 4
    - Use the num-bigint crate to represent arbitrarily large integers.
    - In attempt 3, I converted the answer to a string
      and added up the values of each digit.
    - In attempt 4, I repeatedly take the remainder after dividing by 10
      and add up all these digits. This appears to be slightly faster.
    - Actually solved in May 2021
    - Copied from a private repo into the CodingExercises repo

---
+++ project_euler/src/bin/problem16_bigint_fast.rs
@@ -0,0 +1,23 @@
+use std::time::Instant;
+use num_bigint::BigUint;
+
+const NUM_REPETITIONS: u32 = 100;
+
+fn main() {
+    let start = Instant::now();
+
+    for rep in 0..NUM_REPETITIONS {
+        let number = BigUint::new(vec![2_u32]).pow(1000);
+        let sum_of_digits: u16 = number
+            .to_radix_le(10)
+            .iter()
+            .map(|digit| *digit as u16)
+            .sum();
+        if rep == 0 {
+            println!("Sum of digits = {}", sum_of_digits);
+        }
+    }
+
+    let duration = start.elapsed();
+    println!("Avg duration: {:?}", duration / NUM_REPETITIONS);
+}
