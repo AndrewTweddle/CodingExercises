1a93ff7529757b47647ac1c6abf044be511f3ae0
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-01-02 23:27:33 -0500

    Solve Project Euler problem 17: (on my own)
    - A more intuitive approach for comparison with the TDD solution
    - Shorter and extremely fast, but fragile and tricky to get right
    - Actually solved in May 2021
    - Copied from a private repo into the CodingExercises repo

---
+++ project_euler/src/bin/problem17.rs
@@ -0,0 +1,41 @@
+use std::time::Instant;
+
+const ONE_TO_NINE: &str = "OneTwoThreeFourFiveSixSevenEightNine";
+const TEN_TO_NINETEEN: &str =
+    "TenElevenTwelveThirteenFourteenFifteenSixteenSeventeenEighteenNineteen";
+const TWENTY_TO_NINETY_IN_TENS: &str = "TwentyThirtyFortyFiftySixtySeventyEightyNinety";
+const HUNDRED: &str = "Hundred";
+const HUNDRED_AND: &str = "HundredAnd";
+const ONE_THOUSAND: &str = "OneThousand";
+
+fn main() {
+    let start = Instant::now();
+
+    let num_repetitions = 1000;
+    for rep in 0..num_repetitions {
+        let one_to_nine_len = ONE_TO_NINE.len();
+        let one_to_ninety_nine_len = TEN_TO_NINETEEN.len()
+            // tens part of 1..9, 20..29, 30..39, ..., 91..99:
+            + 10 * TWENTY_TO_NINETY_IN_TENS.len()
+            // units part of 1..9, 20..29, 30..39, ..., 90..99 with zeroes blank:
+            + 9 * one_to_nine_len;
+        let one_to_one_thousand_len
+            = one_to_nine_len
+            // 100, 200, 300, ..., 900:
+            + 9 * HUNDRED.len()
+            // tens and units of 1..99, 101..199, 201..299, ..., 901..999:
+            + 10 * one_to_ninety_nine_len
+            + 99 * (
+                // hundreds digit of 1..99, 101..199, 201..299, ..., 901..999:
+                one_to_nine_len
+                // Prepend "hundred and" to each of "one" to "nine":
+                 + 9 * HUNDRED_AND.len())
+            + ONE_THOUSAND.len();
+        if rep == 0 {
+            println!("Letters in 1 to 1000 written in words: {}", one_to_one_thousand_len);
+        }
+    }
+    let duration = start.elapsed();
+    println!("Total duration: {:?}", duration);
+    println!("Average duration per repetition: {:?}", duration / num_repetitions);
+}
