d7b56a85b29f75dcce83537709741a835dd4264e
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-05-29 23:35:02 -0400

    cryptopals: solve set 1 challenge 5

---
+++ cryptopals/set1/src/bin/challenge5.rs
@@ -0,0 +1,21 @@
+use set1::ciphers::encrypt_using_repeating_key_xor;
+use set1::hex::bytes_to_hex_str;
+
+fn main() {
+    let input = "Burning 'em, if you ain't quick and nimble\n\
+                 I go crazy when I hear a cymbal";
+    let key = "ICE";
+    let encrypted_bytes = encrypt_using_repeating_key_xor(input, key);
+    let output = bytes_to_hex_str(&encrypted_bytes);
+
+    let expected_output = "0b3637272a2b2e63622c2e69692a23693a2a3\
+                           c6324202d623d63343c2a2622632427276527\
+                           2a282b2f20430a652e2c652a3124333a653e2\
+                           b2027630c692b20283165286326302e27282f";
+    assert_eq!(
+        output, expected_output,
+        "encrypted output should equal expected output"
+    );
+
+    println!("Encrypted text in hex: {}", output);
+}
