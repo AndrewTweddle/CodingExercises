117debdaea07bf085e23a60d2bc221a59e09a3eb
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-08-08 23:28:49 -0400

    Improve Project Euler problem 36: attempt 3

--- project_euler/src/bin/problem36_loop_over_3_digits.rs
+++ project_euler/src/bin/problem36_loop_over_3_digits.rs
@@ -8,22 +8,23 @@ fn main() {
     for rep in 0..NUM_REPETITIONS {
         let mut total: u64 = 0;
 
-        // digit0 is the lowest (and highest) digit in the palindromic representation
-        // ignore numbers that end in 0 in decimal or binary...
-        for digit0 in [1, 3, 5, 7, 9] {
-            add_if_binary_palindrome(digit0, &mut total); // 1 digit palindrome
-            add_if_binary_palindrome(11 * digit0, &mut total); // 2 digit palindrome
+        // a is the lowest (and highest) digit in the decimal palindromic representation.
+        // Ignore numbers that end in 0 in binary (i.e. even numbers),
+        // since the binary palindrome must not start with zero...
+        for a in [1, 3, 5, 7, 9] {
+            add_if_binary_palindrome(a, &mut total); // 1 digit palindrome - "a"
+            add_if_binary_palindrome(11 * a, &mut total); // 2 digit palindrome - "aa"
 
-            for digit1 in 0..10 {
-                add_if_binary_palindrome(101 * digit0 + 10 * digit1, &mut total); // 3 digits
-                add_if_binary_palindrome(1001 * digit0 + 110 * digit1, &mut total); // 4 digits
+            for b in 0..10 {
+                add_if_binary_palindrome(101 * a + 10 * b, &mut total); // 3 digits - "aba"
+                add_if_binary_palindrome(1001 * a + 110 * b, &mut total); // 4 digits - "abba"
 
-                for digit2 in 0..10 {
-                    let five_digit_palindrome = 10001 * digit0 + 1010 * digit1 + 100 * digit2;
-                    add_if_binary_palindrome(five_digit_palindrome, &mut total);
+                for c in 0..10 {
+                    let abcba = 10001 * a + 1010 * b + 100 * c; // 5 digits
+                    add_if_binary_palindrome(abcba, &mut total);
 
-                    let six_digit_palindrome = 100001 * digit0 + 10010 * digit1 + 1100 * digit2;
-                    add_if_binary_palindrome(six_digit_palindrome, &mut total);
+                    let abccba = 100001 * a + 10010 * b + 1100 * c; // 6 digits
+                    add_if_binary_palindrome(abccba, &mut total);
                 }
             }
         }
@@ -46,7 +47,7 @@ fn add_if_binary_palindrome(n: u32, total: &mut u64) {
         if rem % 2 != 0 {
             bin_rev |= 1;
         }
-        rem /= 2;
+        rem >>= 1;
     }
     if bin_rev == n {
         *total += n as u64;
