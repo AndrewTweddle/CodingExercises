50e80f43a3d60b5f56bd9db702b1d2ffaa11cdfa
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-10-31 00:28:56 -0400

    CodingDojo: Bowling Scorer: Rust: error checking
    - Add thiserror crate to cargo.toml
    - Define an error enum instead of using Strings
    - Add extra validation patterns to pattern_scorer.rs
    - Add more unit tests to test many more invalid patterns

--- Katas/CodingDojo/Bowling/rust_bowl/src/lib.rs
+++ Katas/CodingDojo/Bowling/rust_bowl/src/lib.rs
@@ -1,10 +1,45 @@
 mod pattern_scorer;
 
+use thiserror::Error;
+
 pub type Score = u16;
 pub type Frame = u8;
 pub type PinCount = u8;
 
-#[derive(Debug)]
+#[derive(Error, Debug, PartialEq)]
+pub enum BowlingScorerError {
+    #[error("Unrecognized symbol: {0}")]
+    UnrecognizedSymbol(char),
+    #[error("Invalid pattern: Next throw cannot be a spare")]
+    FirstOf2ThrowsCannotBeASpare,
+    #[error("Invalid pattern: the throw after a strike cannot be a spare")]
+    ThrowAfterAStrikeCannotBeASpare,
+    #[error("Invalid pattern: the first ball of frame {0} is a spare")]
+    FirstThrowOfAFrameCannotBeASpare(Frame),
+    #[error("Invalid pattern: the second ball of frame {0} is a strike")]
+    SecondThrowInFrameCannotBeAStrike(Frame),
+    #[error("Invalid frame {0}")]
+    InvalidFrame(Frame),
+    #[error("Invalid pattern: no more throws in frame {0}")]
+    NoMoreThrowsInFrame(Frame),
+    #[error("Invalid pattern: not enough throws in frame {0}")]
+    NotEnoughThrowsInFrame(Frame),
+    #[error("Invalid pattern: no more throws after spare in frame {0}")]
+    NoMoreThrowsAfterSpareInFrame(Frame),
+    #[error("Invalid pattern: not enough throws after strike in frame {0}")]
+    NotEnoughThrowsAfterStrikeInFrame(Frame),
+    #[error("Invalid pattern: two throws in a frame {0} add up to {1}")]
+    ThrowsInFrameAddUpToTenOrMore(Frame, Score),
+    #[error("Invalid pattern: a spare in frame 10 cannot be followed by another spare")]
+    SpareCannotFollowASpareInFrame10,
+    #[error("Invalid pattern at frame {frame}: {all_remaining_throws:?}")]
+    InvalidPatternInFrame {
+        frame: Frame,
+        all_remaining_throws: Vec<Throw>,
+    },
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Throw {
     Pins(PinCount),
     Spare,
@@ -32,34 +67,32 @@ impl Throw {
     }
 }
 
-fn char_to_throw(ch: char) -> Result<Throw, String> {
+fn char_to_throw(ch: char) -> Result<Throw, BowlingScorerError> {
     match ch {
         '0'..='9' => Ok(Throw::Pins((ch as PinCount) - b'0')),
         '-' => Ok(Throw::Pins(0)),
         '/' => Ok(Throw::Spare),
         'X' => Ok(Throw::Strike),
-        _ => Err(format!("Unrecognized symbol: {}", ch)),
+        _ => Err(BowlingScorerError::UnrecognizedSymbol(ch)),
     }
 }
 
-pub fn convert_symbols_to_throws(symbols: &str) -> Result<Vec<Throw>, String> {
-    let result: Result<Vec<Throw>, String> = symbols.chars().map(char_to_throw).collect();
+pub fn convert_symbols_to_throws(symbols: &str) -> Result<Vec<Throw>, BowlingScorerError> {
+    let result = symbols.chars().map(char_to_throw).collect();
     result
 }
 
-pub fn score_next_2_throws(throw1: &Throw, throw2: &Throw) -> Result<Score, String> {
+pub fn score_next_2_throws(throw1: &Throw, throw2: &Throw) -> Result<Score, BowlingScorerError> {
     match (throw1, throw2) {
-        (Throw::Spare, _) => Err("Invalid pattern: the next throw cannot be a spare".into()),
-        (Throw::Strike, Throw::Spare) => {
-            Err("Invalid pattern: the throw after a strike cannot be a spare".into())
-        }
+        (Throw::Spare, _) => Err(BowlingScorerError::FirstOf2ThrowsCannotBeASpare),
+        (Throw::Strike, Throw::Spare) => Err(BowlingScorerError::ThrowAfterAStrikeCannotBeASpare),
         (_, Throw::Spare) => Ok(10),
         (throw1, throw2) => Ok(throw1.score() + throw2.score()),
     }
 }
 
 pub trait BowlingScorer {
-    fn score(throws: &str) -> Result<Score, String>;
+    fn score(throws: &str) -> Result<Score, BowlingScorerError>;
 }
 
 #[cfg(test)]
@@ -109,12 +142,48 @@ mod tests {
     #[test]
     fn check_sum_of_2_throws_where_first_is_a_spare() {
         let score = score_next_2_throws(&Throw::Spare, &Throw::Pins(4));
-        assert!(score.is_err());
+        assert_eq!(score, Err(BowlingScorerError::FirstOf2ThrowsCannotBeASpare));
+    }
+
+    #[test]
+    fn check_sum_of_2_throws_where_a_spare_precedes_a_strike() {
+        let score = score_next_2_throws(&Throw::Spare, &Throw::Strike);
+        assert_eq!(score, Err(BowlingScorerError::FirstOf2ThrowsCannotBeASpare));
     }
 
     #[test]
     fn check_sum_of_2_throws_where_a_spare_follows_a_strike() {
-        let score = score_next_2_throws(&Throw::Spare, &Throw::Strike);
-        assert!(score.is_err());
+        let score = score_next_2_throws(&Throw::Strike, &Throw::Spare);
+        assert_eq!(score, Err(BowlingScorerError::ThrowAfterAStrikeCannotBeASpare));
+    }
+
+    #[test]
+    pub fn test_converting_all_valid_symbols() {
+        let symbols = "-0123456789/X";
+        let result = convert_symbols_to_throws(symbols);
+
+        let expected_throws = vec![
+            Throw::Pins(0),
+            Throw::Pins(0),
+            Throw::Pins(1),
+            Throw::Pins(2),
+            Throw::Pins(3),
+            Throw::Pins(4),
+            Throw::Pins(5),
+            Throw::Pins(6),
+            Throw::Pins(7),
+            Throw::Pins(8),
+            Throw::Pins(9),
+            Throw::Spare,
+            Throw::Strike,
+        ];
+        assert_eq!(result, Ok(expected_throws));
+    }
+
+    #[test]
+    pub fn test_converting_invalid_symbols() {
+        let symbols = "2-X1/*";
+        let result = convert_symbols_to_throws(symbols);
+        assert_eq!(result, Err(BowlingScorerError::UnrecognizedSymbol('*')));
     }
 }
