a2f5dc333d1e56f8e7958e6cbe2ebfc987b4c717
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-12-03 18:26:16 -0500

    Improve AOC 2022 day 3 part 2: Python

--- AdventOfCode/aoc2022/src/python/aoc2022_day3_part2.py
+++ AdventOfCode/aoc2022/src/python/aoc2022_day3_part2.py
@@ -3,4 +3,4 @@ with open("../../data/day3_input.txt", "r") as input_file:
     groups = [tuple(elves[i:i+3]) for i in range(0, len(elves), 3)]
     badges = [[badge for badge in elf0 if badge in elf1 and badge in elf2][0] for (elf0, elf1, elf2) in groups]
     priorities = [(1 + item - ord('a') if item >= ord('a') else 27 + item - ord('A')) for item in badges]
-    print(f"AOC 2022: day 3, part 2: {sum(priorities)}")
+    print("AOC 2022: day 3, part 2:", sum(priorities))
