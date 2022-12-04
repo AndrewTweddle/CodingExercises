d538f54ec215589431be0151a3d8ac1ebf841cd3
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2022-12-04 12:43:28 -0500

    Solve AOC 2022 day 4 part 1 and 2 in Python

---
+++ AdventOfCode/aoc2022/src/python/aoc2022_day4_part1and2.py
@@ -0,0 +1,14 @@
+import re
+
+with open("../../data/day4_input.txt", "r") as input_file:
+    part1 = 0
+    part2 = 0
+    for line in input_file.readlines():
+        parts = re.split(r'[-,]', line.rstrip())
+        l_start, l_end, r_start, r_end = [int(part) for part in parts]
+        if (l_start <= r_start and l_end >= r_end) or (l_start >= r_start and l_end <= r_end):
+            part1 += 1
+        if l_start <= r_end and r_start <= l_end:
+            part2 += 1
+    print("AOC 2022: day 3, part 1:", part1)
+    print("AOC 2022: day 3, part 2:", part2)
