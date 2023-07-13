5c6dc50c5c1c77b2a3e6fc85f8e375aa270db5b3
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2023-07-13 01:31:38 -0400

    Project Euler problem 46 v4, 5: clippy & Rustfmt

--- project_euler/src/bin/problem46_v5.rs
+++ project_euler/src/bin/problem46_v5.rs
@@ -1,5 +1,3 @@
-use num::integer::Roots;
-use std::collections::HashSet;
 use std::time::Instant;
 
 const REPETITIONS: u32 = 1000;
@@ -25,12 +23,9 @@ fn main() {
 }
 
 fn solve() -> Option<u64> {
-    for i in (3..).step_by(2) {
-        if !is_odd_prime(i) && !composite_satisfies_conjecture(i) {
-            return Some(i);
-        }
-    }
-    None
+    (3..)
+        .step_by(2)
+        .find(|&i| !is_odd_prime(i) && !composite_satisfies_conjecture(i))
 }
 
 #[inline]
