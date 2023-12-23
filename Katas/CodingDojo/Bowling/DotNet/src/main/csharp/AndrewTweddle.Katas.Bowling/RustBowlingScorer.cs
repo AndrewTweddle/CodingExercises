f95d3e787a20d922157138b03c7079c3dabbe00a
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2023-12-23 00:22:56 -0500

    CodingDojo: Bowling Scorer: Rust/C# FFI: add todo

--- Katas/CodingDojo/Bowling/DotNet/src/main/csharp/AndrewTweddle.Katas.Bowling/RustBowlingScorer.cs
+++ Katas/CodingDojo/Bowling/DotNet/src/main/csharp/AndrewTweddle.Katas.Bowling/RustBowlingScorer.cs
@@ -7,8 +7,9 @@ public class RustBowlingScorer: IBowlingScorer
     public int Calculate(string throwSymbols)
     {
         return score_with_rust(throwSymbols);
+        // TODO: Check for negative scores (signalling an error), and throw a suitable exception
     }
     
     [DllImport("../../../../../../../../rust_bowl/target/debug/librust_bowl.so")]
-    public static extern Int32 score_with_rust([MarshalAs(UnmanagedType.LPUTF8Str)] string throws);
+    static extern Int32 score_with_rust([MarshalAs(UnmanagedType.LPUTF8Str)] string throws);
 }
