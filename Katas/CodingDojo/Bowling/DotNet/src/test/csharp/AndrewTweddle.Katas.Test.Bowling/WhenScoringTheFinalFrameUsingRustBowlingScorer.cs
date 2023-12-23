e7df9602cec129f8e5ab914e35734841f782b2c3
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2023-12-22 22:07:29 -0500

    CodingDojo: Bowling Scorer: C#: wrap Rust scorer

---
+++ Katas/CodingDojo/Bowling/DotNet/src/test/csharp/AndrewTweddle.Katas.Test.Bowling/WhenScoringTheFinalFrameUsingRustBowlingScorer.cs
@@ -0,0 +1,13 @@
+﻿using AndrewTweddle.Katas.Bowling;
+
+namespace AndrewTweddle.Katas.Test.Bowling
+{
+    [TestClass]
+    public class WhenScoringTheFinalFrameUsingRustBowlingScorer : BaseForWhenScoringTheFinalFrame
+    {
+        protected override IBowlingScorer GetScorer()
+        {
+            return new RustBowlingScorer();
+        }
+    }
+}
