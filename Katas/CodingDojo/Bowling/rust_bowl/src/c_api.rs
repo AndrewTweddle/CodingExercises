c7429b3b17d07755972756059c93993e7a519c07
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2023-12-22 22:06:36 -0500

    CodingDojo: Bowling Scorer: Rust: C API (FFI)

---
+++ Katas/CodingDojo/Bowling/rust_bowl/src/c_api.rs
@@ -0,0 +1,28 @@
+use crate::pattern_scorer;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+#[repr(i32)]
+enum ThrowsErrorCode {
+    NullPointer = -1,
+    InvalidUtf8 = -2,
+    InvalidPattern = -3,
+}
+
+#[no_mangle]
+pub extern "C" fn score_with_rust(throws: *const c_char) -> i32 {
+    if throws.is_null() {
+        return ThrowsErrorCode::NullPointer as i32;
+    }
+
+    let throws_c_str = unsafe { CStr::from_ptr(throws) };
+
+    throws_c_str
+        .to_str()
+        .map_or(ThrowsErrorCode::InvalidUtf8 as i32, |throws_str| {
+            pattern_scorer::score_bowling_throws(throws_str)
+                .map_or(ThrowsErrorCode::InvalidPattern as i32, |score_u16| {
+                    score_u16 as i32
+                })
+        })
+}
