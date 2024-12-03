8741726833986f50d7fb8c43f0146324d0124b7f
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2024-12-03 00:17:37 -0500

    AOC 2024: day 2 part 2: Rust: neaten up code

--- AdventOfCode/aoc2024_rs/src/bin/day2_part2.rs
+++ AdventOfCode/aoc2024_rs/src/bin/day2_part2.rs
@@ -15,41 +15,50 @@ fn solve(contents: &str) -> usize {
                 .map(|lvl_str| lvl_str.parse::<i64>().unwrap())
                 .collect();
 
-            // Treat the cases of skipping the first or second level separately,
+            // Treat the cases of dampening the first or second level separately,
             // since these define whether the levels are increasing or decreasing.
-            // Additionally, all future checks will take triples of 3 adjacent levels
-            // and will only check whether the 3rd level needs to be dampened.
-            // (The 2nd and 3rd numbers will be checked, unless the second was dampened in a
-            // previous round, in which case the 1st and 3rd levels in the triple will be checked.)
-            // So these cases also ensure that each pair from the first triple will get checked.
+            // (This also helps to simplify later code by checking the first triple separately.)
             [(Some(0), 1_usize, 2_usize), (Some(1), 0, 2), (None, 0, 1)]
                 .iter()
-                .any(|&(initial_skip_index, first, second)| {
-                    let mut skip_index = initial_skip_index;
-                    let initial_diff = levels[second] - levels[first];
-
-                    // Check the safety of the first pair of numbers chosen from the first triple:
-                    if initial_diff < -3 || initial_diff == 0 || initial_diff > 3 {
-                        false
-                    } else {
-                        let (lo, hi) = if initial_diff >= 0 { (1, 3) } else { (-3, -1) };
-                        levels.windows(3).enumerate().all(|(i, triple)| {
-                            match skip_index {
-                                Some(si) if si == i + 1 => is_safe(triple[0], triple[2], lo, hi),
-                                Some(_) => is_safe(triple[1], triple[2], lo, hi),
-                                None => {
-                                    if !is_safe(triple[1], triple[2], lo, hi) {
-                                        // Skip the last level in the triple
-                                        skip_index = Some(i + 2)
-                                    }
-                                    true
-                                }
-                            }
-                        })
-                    }
+                .any(|&(initial_damp_index, first, second)| {
+                    is_report_safe(&levels, initial_damp_index, first, second)
                 })
         })
         .count()
+}
+
+fn is_report_safe(
+    levels: &[i64],
+    mut damp_index: Option<usize>,
+    first: usize,
+    second: usize,
+) -> bool {
+    let initial_diff = levels[second] - levels[first];
+
+    // Check the safety of the first pair of numbers chosen from the first triple:
+    if initial_diff < -3 || initial_diff == 0 || initial_diff > 3 {
+        false
+    } else {
+        let (lo, hi) = if initial_diff >= 0 { (1, 3) } else { (-3, -1) };
+
+        // Check triples of 3 adjacent levels (using a sliding window).
+        // Only check whether the 3rd level needs to be dampened by comparing it to the 2nd level,
+        // or in the case that the 2nd level has been dampened compare it to the 1st level.
+        // If it's not safe, and damp_index is still None, then dampen (i.e. skip) the 3rd level.
+        levels.windows(3).enumerate().all(|(i, triple)| {
+            match damp_index {
+                Some(di) if di == i + 1 => is_safe(triple[0], triple[2], lo, hi),
+                Some(_) => is_safe(triple[1], triple[2], lo, hi),
+                None => {
+                    if !is_safe(triple[1], triple[2], lo, hi) {
+                        // Skip the last level in the triple
+                        damp_index = Some(i + 2)
+                    }
+                    true
+                }
+            }
+        })
+    }
 }
 
 #[inline(always)]
